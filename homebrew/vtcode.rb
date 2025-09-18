e5673987a32a5e82101bdd02355f669247e9ab76
Vinh Nguyen <vinhnguyen2308@gmail.com>
2025-09-19 00:06:27 +0700

    chore: update homebrew formula to version 0.8.2

--- homebrew/vtcode.rb
+++ homebrew/vtcode.rb
@@ -1,15 +1,15 @@
 class Vtcode < Formula
   desc "A Rust-based terminal coding agent with modular architecture"
   homepage "https://github.com/vinhnx/vtcode"
-  version "0.8.1"
+  version "0.8.2"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/vinhnx/vtcode/releases/download/v#{version}/vtcode-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "CHANGE_THIS_SHA256_AFTER_FIRST_RELEASE" # Calculate: shasum -a 256 vtcode-v0.8.1-aarch64-apple-darwin.tar.gz
+      sha256 "CHANGE_THIS_SHA256_AFTER_FIRST_RELEASE" # Calculate: shasum -a 256 vtcode-v0.8.2-aarch64-apple-darwin.tar.gz
     else
       url "https://github.com/vinhnx/vtcode/releases/download/v#{version}/vtcode-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "CHANGE_THIS_SHA256_AFTER_FIRST_RELEASE" # Calculate: shasum -a 256 vtcode-v0.8.1-x86_64-apple-darwin.tar.gz
+      sha256 "CHANGE_THIS_SHA256_AFTER_FIRST_RELEASE" # Calculate: shasum -a 256 vtcode-v0.8.2-x86_64-apple-darwin.tar.gz
     end
   end
 
